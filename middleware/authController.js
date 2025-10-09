ecb2713980957bc0686bae4d033311e9139399b3
Adnaan Janees <adnaanjanees0@gmail.com>
2025-10-10 00:06:08 +0530

    authorization for APIs

--- middleware/authController.js
+++ middleware/authController.js
@@ -1,19 +1,32 @@
 import jwt from "jsonwebtoken";
 
-export const authUser = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+const authUser = (req, res, next) => {
+  const requestToken = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Authorization token missing or invalid" });
+  if (!requestToken || !requestToken.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Authorization token is invalid" });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const hashed = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = hashed; // now req.user.role and req.user.id are available
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = data; 
     next();
   } catch (error) {
     return res.status(403).json({ message: "Invalid or expired token" });
   }
 };
+
+const validateUser = (...roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return res
+          .status(403)
+          .json({ message: "Access denied" });
+      }
+      next();
+    };
+  };
+  
+export{authUser,validateUser}
--- routes/busRoute.js
+++ routes/busRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { addBus, getAllBuses, editBus, deleteBus, getBusById } from "../controllers/busController.js";
+import { authUser, validateUser } from "../middleware/authController.js";
 
 const router = express.Router();
 
-router.post("/", addBus);
-router.get("/", getAllBuses);
-router.put("/:id", editBus);
-router.delete("/:id", deleteBus);
-router.get("/:id", getBusById);
+router.post("/", authUser, validateUser("Admin", "Operator"), addBus);
+router.get("/", authUser, validateUser("Admin", "Operator"), getAllBuses);
+router.put("/:id", authUser, validateUser("Admin", "Operator"), editBus);
+router.delete("/:id", authUser, validateUser("Admin", "Operator"), deleteBus);
+router.get("/:id", authUser, validateUser("Admin", "Operator"), getBusById);
 
 export default router;
